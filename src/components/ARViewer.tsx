cf92f928c7d14acb8be30a09cbb8842b79022d72
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2025-05-19 18:14:50 +0100

    fixed ts errors

--- src/components/ARViewer.tsx
+++ src/components/ARViewer.tsx
@@ -3,10 +3,26 @@ import { Canvas } from "@react-three/fiber";
 import { ARButton, XR } from "@react-three/xr";
 import { useGLTF } from "@react-three/drei";
 import { createXRStore } from "@react-three/xr";
+import { MeshStandardMaterial } from "three";
 import model from "../assets/model.glb";
+import * as THREE from "three";
 
 const ARModel: React.FC = () => {
   const { scene } = useGLTF(model);
+
+  // Ensure that materials are applied correctly
+  scene.traverse((obj) => {
+    if ((obj as THREE.Mesh).isMesh) {
+      const mesh = obj as THREE.Mesh;
+      if (mesh.material) {
+        mesh.material = new MeshStandardMaterial({
+          color: 0xffffff,
+          metalness: 0.5,
+          roughness: 0.5,
+        });
+      }
+    }
+  });
 
   return <primitive object={scene} scale={[0.5, 0.5, 0.5]} />;
 };
@@ -16,7 +32,6 @@ const ARViewer: React.FC = () => {
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      {/* AR Button to enter AR mode */}
       <ARButton store={store} />
       <Canvas>
         <XR store={store}>
